1c8bc54b0e37763385b44c3fb1a87cce16e47698
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-04-26 23:53:50 +0900

    environment setting

---
+++ blackmagic/src/main/java/com/example/blackmagic/BlackmagicApplication.java
@@ -0,0 +1,13 @@
+package com.example.blackmagic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackmagicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlackmagicApplication.class, args);
+	}
+
+}
---
+++ blackmagic/src/test/java/com/example/blackmagic/BlackmagicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blackmagic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlackmagicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
