7fe6962ab92c2fcbc0f298de4dbd6d68ceea4ffc
JaeyoungAhn <quaki@naver.com>
2023-04-27 14:03:15 +0900

    created MVC structure

--- blackmagic/src/main/java/com/sms/blackmagic/BlackmagicApplication.java
+++ blackmagic/src/main/java/com/sms/blackmagic/BlackmagicApplication.java
@@ -1,4 +1,4 @@
-package com.example.blackmagic;
+package com.sms.blackmagic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
