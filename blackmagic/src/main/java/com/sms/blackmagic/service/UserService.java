ceefeccbe3943aef589a9ebd98fc8254bc6e6b10
JaeyoungAhn <quaki@naver.com>
2023-05-17 20:00:16 +0900

    added getAllUsers() for User Entity

--- blackmagic/src/main/java/com/sms/blackmagic/service/UserService.java
+++ blackmagic/src/main/java/com/sms/blackmagic/service/UserService.java
@@ -1,10 +1,12 @@
 package com.sms.blackmagic.service;
 
 import com.sms.blackmagic.dto.UserDTO;
+import java.util.List;
 
 public interface UserService {
     UserDTO createUser(UserDTO userDTO);
     UserDTO updateUser(UserDTO userDTO);
     void deleteUser(Integer userId);
     UserDTO findUserById(Integer userId);
+    List<UserDTO> findAllUsers();
 }
