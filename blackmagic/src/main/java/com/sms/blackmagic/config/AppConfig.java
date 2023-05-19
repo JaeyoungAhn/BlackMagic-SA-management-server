ebdd8b252e7e7ee126996d11e3291425ee6d73ba
thumbzzero <thumbzzero@gmail.com>
2023-05-19 22:36:01 +0900

    CrossOrigin  setting

--- blackmagic/src/main/java/com/sms/blackmagic/config/AppConfig.java
+++ blackmagic/src/main/java/com/sms/blackmagic/config/AppConfig.java
@@ -2,6 +2,7 @@ package com.sms.blackmagic.config;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
--- blackmagic/src/main/java/com/sms/blackmagic/controller/CompanyController.java
+++ blackmagic/src/main/java/com/sms/blackmagic/controller/CompanyController.java
@@ -10,6 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:3000")
 public class CompanyController {
     private final CompanyService companyService;
 
