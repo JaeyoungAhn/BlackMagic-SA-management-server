2df4cf8c22a2b1cae151cc2366dfafc1f1978dc1
shin-jisong <86762272+shin-jisong@users.noreply.github.com>
2023-05-23 01:20:30 +0900

    create record api

--- blackmagic/src/main/java/com/sms/blackmagic/service/RecordService.java
+++ blackmagic/src/main/java/com/sms/blackmagic/service/RecordService.java
@@ -5,6 +5,10 @@ import com.sms.blackmagic.repository.RecordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 
 @Service
 @RequiredArgsConstructor
@@ -20,6 +24,23 @@ public class RecordService {
     public Record readRecord(Long recordId) {
         Record record = recordRepository.findByRecordId(recordId);
         return record;
+    }
+
+    public List<Record> getRecordList() {
+        List<Record> recordList = recordRepository.findAll();
+        return recordList;
+    }
+
+    public List<Record> getRecordListByCompany(Integer companyId) {
+        List<Record> recordList = recordRepository.findAll();
+        List<Record> recordListByCompany = new ArrayList<>();
+
+        for(Record record: recordList) {
+            if(Objects.equals(record.getCompany().getCompanyId(), companyId))
+                recordListByCompany.add(record);
+        }
+
+        return recordListByCompany;
     }
 
     public void deleteRecord(Long recordId) {
