608df5c74d1bef625e8d1d1333cfb5e6f75c0803
JaeyoungAhn <quaki@naver.com>
2023-05-24 12:33:17 +0900

    Allow preflight OPTIONS reqeust to all

--- blackmagic/src/main/java/com/sms/blackmagic/config/SecurityConfigurer.java
+++ blackmagic/src/main/java/com/sms/blackmagic/config/SecurityConfigurer.java
@@ -14,6 +14,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsUtils;
 
 @Configuration
 @EnableWebSecurity
@@ -44,6 +45,7 @@ public class SecurityConfigurer {
             .sessionManagement()
             .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
             .and().authorizeHttpRequests()
+            .requestMatchers(CorsUtils::isPreFlightRequest).permitAll()
             .requestMatchers(HttpMethod.POST, "/user/**").permitAll() // signup, login, logout
             .requestMatchers("/user/**").hasRole("ADMIN")
             .requestMatchers("/company/**").hasRole("ADMIN")
